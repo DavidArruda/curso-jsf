ad80924c73dc18605c08eccd359119e7f9d4aa94
DavidArruda <deh0_@hotmail.com>
2019-12-28 10:46:29 -0200

    metodo para teste JPA

---
+++ src/test/java/br/com/cursojsf/TestJPA.java
@@ -0,0 +1,15 @@
+package br.com.cursojsf;
+
+
+import javax.persistence.Persistence;
+
+
+public class TestJPA {
+	
+	public static void main(String[] args) {
+		Persistence.createEntityManagerFactory("meuprimeiroprojetojsf");
+		//HibernatePersistenceProvider  provider = new HibernatePersistenceProvider();
+		//provider.createEntityManagerFactory("meuprimeiroprojetojsf", null);
+	}
+
+}
