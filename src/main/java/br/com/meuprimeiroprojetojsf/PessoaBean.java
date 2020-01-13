f96bdaee64f21d7488c21738603c467f80712437
DavidArruda <deh0_@hotmail.com>
2020-01-13 12:34:51 -0300

    Criação do metodo para deslogar usuário e metodo para consultar webservice de CEP

--- src/main/java/br/com/meuprimeiroprojetojsf/PessoaBean.java
+++ src/main/java/br/com/meuprimeiroprojetojsf/PessoaBean.java
@@ -1,5 +1,10 @@
 package br.com.meuprimeiroprojetojsf;
 
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -9,6 +14,10 @@ import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
 import javax.faces.context.ExternalContext;
 import javax.faces.context.FacesContext;
+import javax.faces.event.AjaxBehaviorEvent;
+import javax.servlet.http.HttpServletRequest;
+
+import com.google.gson.Gson;
 
 import br.com.dao.DaoGeneric;
 import br.com.entidades.Pessoa;
@@ -26,28 +35,33 @@ public class PessoaBean {
 
 	public String salvar() {
 		pessoa = daoGeneric.merge(pessoa);
-		
+
 		listarPessoas();
-		
+
 		mostrarMsg("Salvo com sucesso");
-		
+
 		return "";
 	}
-	
+
 	private void mostrarMsg(String msg) {
-		
+
 		FacesContext context = FacesContext.getCurrentInstance();
-		
+
 		FacesMessage message = new FacesMessage(msg);
-		
+
 		context.addMessage(null, message);
 	}
 
 	public String novo() {
 		pessoa = new Pessoa();
-		return "";	
+		return "";
 	}
-	
+
+	public String limpar() {
+		pessoa = new Pessoa();
+		return "";
+	}
+
 	public String delete() {
 		daoGeneric.deletePorId(pessoa);
 		pessoa = new Pessoa();
@@ -55,37 +69,96 @@ public class PessoaBean {
 		mostrarMsg("Removido com sucesso");
 		return "";
 	}
+
+	public void pesquisaCep(AjaxBehaviorEvent event) {
+
+		try {
+
+			URL url = new URL("https://viacep.com.br/ws/" + pessoa.getCep() + "/json/");
+
+			URLConnection connection = url.openConnection();
+
+			InputStream is = connection.getInputStream();
+
+			BufferedReader br = new BufferedReader(new InputStreamReader(is, "UTF-8"));
+
+			String cep = "";
+
+			StringBuilder json = new StringBuilder();
+
+			while ((cep = br.readLine()) != null) {
+				json.append(cep);
+			}
+
+			Pessoa gson = new Gson().fromJson(json.toString(), Pessoa.class);
+
+			pessoa.setCep(gson.getCep());
+			pessoa.setLogradouro(gson.getLogradouro());
+			pessoa.setComplemento(gson.getComplemento());
+			pessoa.setBairro(gson.getBairro());
+			pessoa.setLocalidade(gson.getLocalidade());
+			pessoa.setUf(gson.getUf());
+			pessoa.setUnidade(gson.getUnidade());
+			pessoa.setIbge(gson.getIbge());
+			pessoa.setGia(gson.getGia());
+
+		} catch (Exception e) {
+			e.printStackTrace();
+			mostrarMsg("Erro ao consultar CEP");
+		}
+	}
 	
+	public String deslogar() {
+		
+		//invalidate do jsf
+		//FacesContext.getCurrentInstance().getExternalContext().invalidateSession();
+		
+		FacesContext context = FacesContext.getCurrentInstance();
+		
+		ExternalContext externalContext = context.getExternalContext();
+		externalContext.getSessionMap().remove("usuarioLogado");
+		
+		HttpServletRequest httpServletRequest = (HttpServletRequest)
+				context.getCurrentInstance().
+		getExternalContext().getRequest();
+		
+		httpServletRequest.getSession().invalidate();
+		
+		return "index.jsf";
+	}
+
 	@PostConstruct
 	public void listarPessoas() {
 		pessoas = daoGeneric.listEntity(Pessoa.class);
 	}
-	
+
 	public String logar() {
 		Pessoa pessoaUser = iDaoPessoa.consultarPessoa(pessoa.getLogin(), pessoa.getSenha());
-		
+
 		if (pessoaUser != null) {
-			
-			//adicionar o usuario na sessao "usuarioLogado"
-			FacesContext facesContext = FacesContext.getCurrentInstance(); // FacesContext contem informações do ambiente de execução 'JSF"
+
+			// adicionar o usuario na sessao "usuarioLogado"
+			FacesContext facesContext = FacesContext.getCurrentInstance(); // FacesContext contem informações do
+																			// ambiente de execução 'JSF"
 			ExternalContext externalContext = facesContext.getExternalContext();
 			externalContext.getSessionMap().put("usuarioLogado", pessoaUser);
 			return "primeirapagina.jsf";
 		}
-		
+
 		return "index.jsf";
 	}
-	
+
 	public boolean permiteAcesso(String acesso) {
-		
-		FacesContext facesContext = FacesContext.getCurrentInstance(); // FacesContext contem informações do ambiente de execução 'JSF"
+
+		FacesContext facesContext = FacesContext.getCurrentInstance(); // FacesContext contem informações do ambiente de
+																		// execução 'JSF"
 		ExternalContext externalContext = facesContext.getExternalContext();
 		Pessoa pessoaUser = (Pessoa) externalContext.getSessionMap().get("usuarioLogado");
-		
+
 		return pessoaUser.getPerfilUser().equals(acesso);
-				
+
 	}
-	
+
 	public List<Pessoa> getPessoas() {
 		return pessoas;
 	}
