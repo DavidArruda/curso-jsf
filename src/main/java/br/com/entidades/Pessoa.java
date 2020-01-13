e1b7e8a3314b286cfcb34514ab78d43e8db40292
DavidArruda <deh0_@hotmail.com>
2020-01-13 12:33:48 -0300

    adicionado atributos para endereço do usuário

--- src/main/java/br/com/entidades/Pessoa.java
+++ src/main/java/br/com/entidades/Pessoa.java
@@ -12,54 +12,144 @@ import javax.persistence.TemporalType;
 
 @Entity
 public class Pessoa implements Serializable {
-	
+
 	private static final long serialVersionUID = 1L;
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long id;
-	
+
 	private String nome;
-	
+
 	private String sobrenome;
-	
+
 	private Integer idade;
-	
+
 	private String sexo;
-	
+
 	private String nivelProgramador;
-	
+
 	private String[] frameworks;
-	
+
 	private Boolean ativo;
-	
+
 	private String login;
-	
+
 	private String senha;
-	
+
 	private String perfilUser;
-	
+
 	private Integer[] linguagens;
-	
+
+	private String cep;
+
+	private String logradouro;
+
+	private String complemento;
+
+	private String bairro;
+
+	private String localidade;
+
+	private String uf;
+
+	private String unidade;
+
+	private String ibge;
+
+	private String gia;
+
 	@Temporal(TemporalType.DATE)
-	private Date dataNascimento;
-	
+	private Date dataNascimento = new Date();
+
 	public Pessoa() {
 
 	}
+
+	public String getLogradouro() {
+		return logradouro;
+	}
+
+	public void setLogradouro(String logradouro) {
+		this.logradouro = logradouro;
+	}
+
+	public String getComplemento() {
+		return complemento;
+	}
+
+	public void setComplemento(String complemento) {
+		this.complemento = complemento;
+	}
+
+	public String getBairro() {
+		return bairro;
+	}
+
+	public void setBairro(String bairro) {
+		this.bairro = bairro;
+	}
+
+	public String getLocalidade() {
+		return localidade;
+	}
+
+	public void setLocalidade(String localidade) {
+		this.localidade = localidade;
+	}
+
+	public String getUf() {
+		return uf;
+	}
 	
+	public void setUf(String uf) {
+		this.uf = uf;
+	}
+
+	public String getUnidade() {
+		return unidade;
+	}
+
+	public void setUnidade(String unidade) {
+		this.unidade = unidade;
+	}
+
+	public String getIbge() {
+		return ibge;
+	}
+
+	public void setIbge(String ibge) {
+		this.ibge = ibge;
+	}
+
+	public String getGia() {
+		return gia;
+	}
+
+	public void setGia(String gia) {
+		this.gia = gia;
+	}
+
+	public String getCep() {
+		return cep;
+	}
+
+	public void setCep(String cep) {
+		this.cep = cep;
+	}
+
 	public Integer[] getLinguagens() {
 		return linguagens;
 	}
-	
+
 	public void setLinguagens(Integer[] linguagens) {
 		this.linguagens = linguagens;
 	}
-	
+
 	public String getNivelProgramador() {
 		return nivelProgramador;
 	}
-	
+
 	public void setNivelProgramador(String nivelProgramador) {
 		this.nivelProgramador = nivelProgramador;
 	}
@@ -103,11 +193,11 @@ public class Pessoa implements Serializable {
 	public void setDataNascimento(Date dataNascimento) {
 		this.dataNascimento = dataNascimento;
 	}
-	
+
 	public String getSexo() {
 		return sexo;
 	}
-	
+
 	public void setSexo(String sexo) {
 		this.sexo = sexo;
 	}
@@ -115,7 +205,7 @@ public class Pessoa implements Serializable {
 	public String[] getFrameworks() {
 		return frameworks;
 	}
-	
+
 	public void setFrameworks(String[] frameworks) {
 		this.frameworks = frameworks;
 	}
@@ -176,5 +266,5 @@ public class Pessoa implements Serializable {
 			return false;
 		return true;
 	}
-	
+
 }
