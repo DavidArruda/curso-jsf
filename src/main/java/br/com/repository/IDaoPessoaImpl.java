b449c99f4bf518cec9e5189604d730277c3d6616
DavidArruda <deh0_@hotmail.com>
2020-01-13 15:40:03 -0300

    Implantação do metodo listarE stados

--- src/main/java/br/com/repository/IDaoPessoaImpl.java
+++ src/main/java/br/com/repository/IDaoPessoaImpl.java
@@ -1,8 +1,13 @@
 package br.com.repository;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.faces.model.SelectItem;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityTransaction;
 
+import br.com.entidades.Estados;
 import br.com.entidades.Pessoa;
 import br.com.jpautil.JPAUtil;
 
@@ -26,6 +31,26 @@ public class IDaoPessoaImpl implements IDaoPessoa {
 		entityManager.close();
 		
 		return pessoa;
+	}
+
+	@Override
+	public List<SelectItem> listaEstados() {
+		
+		List<SelectItem> selectItems = new ArrayList<SelectItem>();
+
+		EntityManager entityManager = JPAUtil.getEntityManager();
+		
+		EntityTransaction entityTransaction = entityManager.getTransaction();
+		entityTransaction.begin();
+		
+		@SuppressWarnings("unchecked")
+		List<Estados> listaEstados = entityManager.createQuery("from Estados").getResultList();
+		
+		for (Estados estado : listaEstados) {
+			selectItems.add(new SelectItem(estado.getId(), estado.getNome()));
+		}
+		
+		return selectItems;
 	}
 
 }
