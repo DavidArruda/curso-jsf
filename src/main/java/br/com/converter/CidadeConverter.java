95e7daf2a1bc65608918cc31c36420db9b473cd6
DavidArruda <deh0_@hotmail.com>
2020-01-31 14:12:14 -0300

    Adicionado CDI aos converter

--- src/main/java/br/com/converter/CidadeConverter.java
+++ src/main/java/br/com/converter/CidadeConverter.java
@@ -2,15 +2,14 @@ package br.com.converter;
 
 import java.io.Serializable;
 
+import javax.enterprise.inject.spi.CDI;
 import javax.faces.component.UIComponent;
 import javax.faces.context.FacesContext;
 import javax.faces.convert.Converter;
 import javax.faces.convert.FacesConverter;
 import javax.persistence.EntityManager;
-import javax.persistence.EntityTransaction;
 
 import br.com.entidades.Cidades;
-import br.com.jpautil.JPAUtil;
 
 @FacesConverter(forClass = Cidades.class, value = "cidadeConverter")
 public class CidadeConverter implements Converter, Serializable {
@@ -19,9 +18,8 @@ public class CidadeConverter implements Converter, Serializable {
 
 	@Override // Retorna o objeto inteiro
 	public Object getAsObject(FacesContext context, UIComponent component, String codigoCidade) {
-		EntityManager entityManager = JPAUtil.getEntityManager();
-		EntityTransaction entityTransaction = entityManager.getTransaction();
-		entityTransaction.begin();
+
+		EntityManager entityManager = CDI.current().select(EntityManager.class).get();
 
 		Cidades cidades = (Cidades) entityManager.find(Cidades.class, Long.parseLong(codigoCidade));
 
